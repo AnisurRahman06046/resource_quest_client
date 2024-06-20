0f49d2a4f7f0f7cd1b59c09c3878be12ac2081c9
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-20 23:34:00 +0600

    sign up page done

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import logo from "../../../public/rq1.png";
+import SideImg from "../../../public/collective-3291989_1280.jpg";
 import Button from "../Button/Button";
 
 function SignUp() {
@@ -9,9 +10,9 @@ function SignUp() {
         <div className="flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
           <div className="w-full">
             <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
-              <div className="g-0 lg:flex lg:flex-wrap">
+              <div className="g-0 lg:flex  lg:flex-wrap">
                 {/* Left column container */}
-                <div className="px-4 md:px-0 lg:w-6/12 bg-brand-color">
+                <div className="px-4 md:px-0 lg:w-6/12 bg-black">
                   <div className="md:mx-6 md:p-12">
                     {/* Logo */}
                     <div className="text-center">
@@ -69,23 +70,16 @@ function SignUp() {
 
                 {/* Right column container with background and description */}
                 <div
-                  className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-e-lg lg:rounded-bl-none"
+                  className="flex items-center  lg:w-6/12  lg:rounded-bl-none"
                   style={{
                     background:
                       "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                    backgroundImage: `url(${SideImg})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
                   }}
                 >
-                  <div className="px-4 py-6 text-white md:mx-6 md:p-12">
-                    <h4 className="mb-6 text-xl font-semibold">
-                      We are more than just a company
-                    </h4>
-                    <p className="text-sm">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                  </div>
+                  {/* <img className="ml-16" src={SideImg} alt="" /> */}
                 </div>
               </div>
             </div>
