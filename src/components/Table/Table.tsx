c0e1e0d6d2f144394bae0f2ced779b93f121355f
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-23 03:24:24 +0600

    table is updated

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -9,19 +9,20 @@ function Table() {
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" className="px-6 py-3">
-                    Product name
+                    Title
                 </th>
                 <th scope="col" className="px-6 py-3">
-                    Color
+                    Link
                 </th>
                 <th scope="col" className="px-6 py-3">
-                    Category
+                    Description
                 </th>
                 <th scope="col" className="px-6 py-3">
-                    Price
+                    Edit
                 </th>
-                <th scope="col" className="px-6 py-3">
-                    Action
+                <th scope="col" className="px-6 flex lg:gap-1 py-3">
+                    <p>Edit/</p>
+                    <p>Delete</p>
                 </th>
             </tr>
         </thead>
